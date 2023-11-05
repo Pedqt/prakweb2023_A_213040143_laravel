79b36a8340e5ab990b2d66ba999e0e45a5a8a17e
pedqt <juliofaridh00@gmail.com>
2023-11-05 20:00:11 +0700

    push tugas

--- coba-laravel/database/seeders/DatabaseSeeder.php
+++ coba-laravel/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Post;
+use App\Models\Category;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +15,96 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(3)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+
+            // User::create([
+            //     'name'=> 'Hail Hilal',
+            //     'email'=> 'hail.hilal@gmail.com',
+            //     'password'=> bcrypt('12345')
+            // ]);
+
+            // User::create([
+            //     'name'=> 'S.Hilal',
+            //     'email'=> 'shilal@gmail.com',
+            //     'password'=> bcrypt('12345')
+            // ]);
+
+            Category::create([
+                'name'=> 'Web programming',
+                'slug'=> 'web-programming'
+            ]);
+
+            Category::create([
+                'name'=> 'Web design',
+                'slug'=> 'web-design'
+            ]);
+
+            Category::create([
+                'name'=> 'Personal',
+                'slug'=> 'personal'   
+            ]);
+
+            Post::factory(20)->create();
+
+        //     Post::create([
+        //         'title' => 'Judul pertamax',
+        //         'slug' => 'judul-pertamax',
+        //         'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        //         Fugit dolores repellat velit pariatur saepe non suscipit rerum, 
+        //         deserunt, quod nulla a ipsa rem numquam, ex culpa ad iste iure labore.',
+        //         'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        //         Alias hic, eligendi iusto maxime quisquam consequatur temporibus beatae. 
+        //         Fugit qui velit tempora expedita itaque! Voluptates, corrupti.',
+        //         'category_id'=> 1,
+        //         'user_id'=> 1
+        //   ]);
+
+        //     Post::create([
+        //         'title' => 'Judul keduax',
+        //         'slug' => 'judul-keduax',
+        //         'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        //         Fugit dolores repellat velit pariatur saepe non suscipit rerum, 
+        //         deserunt, quod nulla a ipsa rem numquam, ex culpa ad iste iure labore.',
+        //         'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        //         Alias hic, eligendi iusto maxime quisquam consequatur temporibus beatae. 
+        //         Fugit qui velit tempora expedita itaque! Voluptates, corrupti.',
+        //         'category_id'=> 1,
+        //         'user_id'=> 1
+        //    ]);
+
+        //     Post::create([
+        //         'title' => 'Judul ketiax',
+        //         'slug' => 'judul-ketiax',
+        //         'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        //         Fugit dolores repellat velit pariatur saepe non suscipit rerum, 
+        //         deserunt, quod nulla a ipsa rem numquam, ex culpa ad iste iure labore.',
+        //         'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        //         Alias hic, eligendi iusto maxime quisquam consequatur temporibus beatae. 
+        //         Fugit qui velit tempora expedita itaque! Voluptates, corrupti.',
+        //         'category_id'=> 2,
+        //         'user_id'=> 1
+        //   ]);
+        
+        //   Post::create([
+        //         'title' => 'Judul keempax',
+        //         'slug' => 'judul-keempax',
+        //         'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        //         Fugit dolores repellat velit pariatur saepe non suscipit rerum, 
+        //         deserunt, quod nulla a ipsa rem numquam, ex culpa ad iste iure labore.',
+        //         'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        //         Alias hic, eligendi iusto maxime quisquam consequatur temporibus beatae. 
+        //         Fugit qui velit tempora expedita itaque! Voluptates, corrupti.',
+        //         'category_id'=> 2,
+        //         'user_id'=> 2
+        //   ]);
+
+
+
     }
 }
--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PostController;
+use App\Models\Category;
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,69 +18,45 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "home"
+        "title" => "home",
+        "active" => "home"
     ]);
 });
 
 Route::get('/about', function () {
     return view('about',[
         "title" => "About",
+        "active"=> "about",
         "name" => "Jullpedd",
         "email" => "juliofaridh00@gmail.com",
         "image" => "ppsadboy.jpeg"
     ]);
 });
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "mr x",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque quisquam illo totam error unde eveniet cupiditate minus suscipit. Cupiditate nulla esse fugit alias illo. Quas suscipit, cumque nulla et ipsum blanditiis voluptatem odit! Voluptas nisi harum, voluptatem asperiores blanditiis dolorum, incidunt soluta est neque esse totam! Natus dolore reiciendis quibusdam, aliquid debitis, velit molestiae ad, autem dicta laborum mollitia? Iste."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "mr c",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque quisquam illo totam error unde eveniet cupiditate minus suscipit. Cupiditate nulla esse fugit alias illo. Quas suscipit, cumque nulla et ipsum blanditiis voluptatem odit! Voluptas nisi harum, voluptatem asperiores blanditiis dolorum, incidunt soluta est neque esse totam! Natus dolore reiciendis quibusdam, aliquid debitis, velit molestiae ad, autem dicta laborum mollitia? Iste."
-        ],
-        ];
-         
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
+Route::get('/blog', [PostController::class, 'index']);
+
+// Halaman Single Post
+Route::get('/posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('/categories', function () {
+    return view('categories', [
+        'title' => 'Post Categories',
+        'active'=> "categories",
+        'categories' => Category::all()
     ]);
 });
 
-//halaman single post
-Route::get('posts/{slug}', function($slug) {
+Route::get('/categories/{category:slug}', function(Category $category) {
+    return view('posts',[
+        'title'=> "post By Author : $category->name",
+        'active'=> "categories",
+        'posts' => $category->posts->load('category', 'author'),
+    ]);
+});
 
-$blog_posts = [
-    [
-        "title" => "Judul Post Pertama",
-        "slug" => "judul-post-pertama",
-        "author" => "gapapa ko",
-        "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque quisquam illo totam error unde eveniet cupiditate minus suscipit. Cupiditate nulla esse fugit alias illo. Quas suscipit, cumque nulla et ipsum blanditiis voluptatem odit! Voluptas nisi harum, voluptatem asperiores blanditiis dolorum, incidunt soluta est neque esse totam! Natus dolore reiciendis quibusdam, aliquid debitis, velit molestiae ad, autem dicta laborum mollitia? Iste."
-    ],
-    [
-        "title" => "Judul Post Kedua",
-        "slug" => "judul-post-kedua",
-        "author" => "jeje",
-        "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque quisquam illo totam error unde eveniet cupiditate minus suscipit. Cupiditate nulla esse fugit alias illo. Quas suscipit, cumque nulla et ipsum blanditiis voluptatem odit! Voluptas nisi harum, voluptatem asperiores blanditiis dolorum, incidunt soluta est neque esse totam! Natus dolore reiciendis quibusdam, aliquid debitis, velit molestiae ad, autem dicta laborum mollitia? Iste."
-    ],
-    ];
-
-$new_post = [];
-foreach($blog_posts as $post) {
-    if($post["slug"] === $slug) {
-        $new_post = $post;
-    }
-}    
-
-
-return view('post', [
-    "title" => "Single Post",
-    "post" => $new_post
-]);
+Route::get('/authors/{author:username}', function(user $author){
+    return view('posts', [
+        'title'=> "Posts By Author : $author->name",
+        'posts' => $author->posts->load('category', 'author'),
+    ]);
 });
