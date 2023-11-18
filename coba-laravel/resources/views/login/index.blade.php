d47fd57e20d0f31056cdcceae45fdb8f1c22bea0
saiifulhilal <hilalasw@gmail.com>
2023-11-18 11:33:56 +0700

    tugas 3 laravel pt.2

--- coba-laravel/resources/views/login/index.blade.php
+++ coba-laravel/resources/views/login/index.blade.php
@@ -6,8 +6,6 @@
         <main class="form-signin">
           <h1 class="h3 mb-3 fw-normal text-center">Please Login</h1>
           <form>
-            <img class="mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-
             <div class="form-floating">
               <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
               <label for="floatingInput">Email address</label>
--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -59,3 +59,7 @@ Route::get('/categories', function () {
         'categories' => Category::all()
     ]);
 });
+
+Route::get('/login', [LoginController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index']);
+Route::post('/register', [RegisterController::class, 'store']);
